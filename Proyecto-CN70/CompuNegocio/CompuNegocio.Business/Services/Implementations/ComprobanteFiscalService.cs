1e7f8e70f16060d4346141a61e2b549481edfbea
Carlos Romero <jockar07@gmail.com>
2023-03-29 08:17:43 -0700

    Se valida si periodicidad es nulo.

--- Proyecto-CN70/CompuNegocio/CompuNegocio.Business/Services/Implementations/ComprobanteFiscalService.cs
+++ Proyecto-CN70/CompuNegocio/CompuNegocio.Business/Services/Implementations/ComprobanteFiscalService.cs
@@ -642,8 +642,13 @@ namespace Aprovi.Business.Services
                 nodoComprobante.Attributes.Append(Schema(xmlDoc, false));
                 if (invoice.idComprobanteOriginal.isValid() || invoice.NotasDeCreditoes.Any(x => x.IsPreSaleCreditNote(invoice)))
                     CFDIRelacionados(xmlDoc, invoice).ForEach(x => nodoComprobante.AppendChild(x));
-                if (invoice.Cliente.rfc == "XAXX010101000" && !invoice.Periodicidad.Equals(null) && invoice.Periodicidad.EsFacturaGlobal) //RVC Pendiente ver como manejar la factura global desde la pantalla de ventas/facturacion
-                    nodoComprobante.AppendChild(CFDIInformacionGlobal(xmlDoc, invoice));
+
+                if (invoice.Periodicidad != null)
+                {
+                    if (invoice.Cliente.rfc == "XAXX010101000" && invoice.Periodicidad.EsFacturaGlobal != null && invoice.Periodicidad.EsFacturaGlobal) //RVC Pendiente ver como manejar la factura global desde la pantalla de ventas/facturacion
+                        nodoComprobante.AppendChild(CFDIInformacionGlobal(xmlDoc, invoice));
+                }
+
                 nodoComprobante.AppendChild(Emisor(xmlDoc, config));
                 nodoComprobante.AppendChild(Receptor(xmlDoc, invoice.Cliente, invoice.UsosCFDI, invoice.Cliente.Domicilio, invoice.Cliente.Regimene));
                 nodoComprobante.AppendChild(Conceptos(xmlDoc, invoice));
